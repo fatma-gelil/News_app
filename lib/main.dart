b597fa8f34f59a21fbdf8ebfcf21cfcf2ce26c2e
Fatma Gelil <93225388+fatma-gelil@users.noreply.github.com>
2024-12-20 23:16:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/widgets/category_card.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
